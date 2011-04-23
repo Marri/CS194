f5f68809c318fb51045300a64df703f8834f016f
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2011-04-23 11:55:49 -0700

    started writing code that displays messages; added methods in the message class

--- message_view.php
+++ message_view.php
@@ -1,6 +1,17 @@
 <?php
 include("./includes/header.php");
 include("./objects/forums.php");
+$userid = "1"; //$_S
+$message_list = array();
+
+$query = "SELECT * FROM messages WHERE to_id = '".$userid."';"
+$results = runDBQuery($query);
+
+while($messages = mysql_fetch_assoc($results)) {
+	$curr_message = Message::CreateMessage($id, $messages['to_id'],$messages['from_id'], $messages['message_type'], $messages['subject'], $message['message'], $message['time_sent'],  $message['is_read'], $message['in_inbox'],  $message['in_outbox']);
+	array_push($message_list, $curr_message);
+}
+
 
 ?>
 <h1> Message View</h1>
--- objects/messaging.php
+++ objects/messaging.php
@@ -2,7 +2,27 @@
 
 class Message {
 	private 
-		id,	to_id, from_id, message_type, subject,message text, time_sent, is_read,	in_inbox, in_outbox;
-	
+		$id,	$to_id, $from_id, $message_type, $subject,message, $time_sent, $is_read,$in_inbox, $in_outbox;
+	public static function CreateMessage($id,	$to_id, $from_id, $message_type, $subject,message, $time_sent, $is_read,$in_inbox, $in_outbox){
+		$curr_message = new Message();
+		$curr_message->id = $id;
+		$curr_message->to_id = $messages['to_id'];
+		$curr_message->from_id = $messages['from_id'];
+		$curr_message->message_type = $messages['message_type'];
+		$curr_message->subject = $messages['subject'];
+		$curr_message->message = $message['message'];
+		$curr_message->time_sent = $message['time_sent'];
+		$curr_message->is_sent = $message['is_read'];
+		$curr_message->in_inbox = $message['in_inbox'];
+		$curr_message->in_outbox = $message['in_outbox'];
+		return $curr_message;
+	}
+	public static function GetMessageFromID($id){
+		$query = "SELECT * FROM messages WHERE message_id='".$id."';"
+		$results = runDBQuery($query);
+		while($messages = mysql_fetch_assoc($results)) {
+			return Message::CreateMessage($id, $messages['to_id'],$messages['from_id'], $messages['message_type'], $messages['subject'], $message['message'], $message['time_sent'],  $message['is_read'], $message['in_inbox'],  $message['in_outbox']);
+		}
+	}
 }
 ?>
