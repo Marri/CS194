f108487e43f33d311449048df7dafaba2f9c66b0
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2011-04-23 11:14:12 -0700

    I'm adding two files related to messaging

---
+++ message_view.php
@@ -0,0 +1,10 @@
+<?php
+include("./includes/header.php");
+include("./objects/forums.php");
+
+?>
+<h1> Message View</h1>
+<?php
+
+include('./includes/footer.php');
+?>
---
+++ objects/messaging.php
@@ -0,0 +1,8 @@
+<?php
+
+class Message {
+	private 
+		id,	to_id, from_id, message_type, subject,message text, time_sent, is_read,	in_inbox, in_outbox;
+	
+}
+?>
