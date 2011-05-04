18042543702bad69ebc2fd7a1ea3902dd3a70a05
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2011-05-04 15:31:19 -0700

    adding notification object

---
+++ objects/notification.php
@@ -0,0 +1,22 @@
+<?php
+class Notification{
+	private
+		$id,
+		$user_id,
+		$type,
+		$unread;
+	
+	public function __construct($note){
+		$this->id = $note['notification_id'];
+		$this->user_id = $note['user_id'];
+		$this->type = $note['notification_type'];
+		$this->unread = $note['unread'];
+	}
+	public function getID(){ return $this->id; }
+	public function getUserID(){ return $this->user_id; }
+	public function getNoteType(){ return $this->type; }
+	public function unread(){ return $this->unread; }
+	
+	
+}
+?>
