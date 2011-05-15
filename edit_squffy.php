a8e8a79dc9618c7f9bf739c7c8ef6a717249d188
Amanda <xxstealthestarsxx@gmail.com>
2011-05-15 01:01:38 -0700

    Inventory defaults to 0, expanding Reorder Trait section

--- edit_squffy.php
+++ edit_squffy.php
@@ -63,61 +63,43 @@ Edit <?php echo $name; ?>
 <tr><th class="content-subheader width50p" colspan="2">Edit Information</th><th class="content-subheader" colspan="4">Reorder Appearance Traits</th></tr>
 
 <tr><td>Name</td><td><input class="width100p" type='text' name="squffy_name" value="<?php echo $name; ?>" /></td>
-<?php
-if($i < $num) {
-	echo '<td class="width125">' . $traits[$i]->getTitle() . '</td><td class="width50"> 
-	<div class="color-box" style="background-color: #' . $traits[$i]->getColor() . '"></div></td>
-	<td>' . $traits[$i]->getColor() .'</td>
-	<td>MOVE</td>';
-	$i++;
-} 
-?>
+<?php $i = showTrait($num, $traits, $i); ?>
 </tr>
 <tr><td class="width150">Available for hire?</td>
 <td><input type='radio' name="hireable" class="hireable" value='y'<?php checked($hireable); ?> /> Yes <input type='radio' name="hireable" class="hireable" value='n'<?php checked(!$hireable); ?> /> No</td>
-<?php
-if($i < $num) {
-	echo '<td class="width125">' . $traits[$i]->getTitle() . '</td><td class="width50"> 
-	<div class="color-box" style="background-color: #' . $traits[$i]->getColor() . '"></div></td>
-	<td>' . $traits[$i]->getColor() .'</td>
-	<td>MOVE</td>';
-	$i++;
-} 
-?>
+<?php $i = showTrait($num, $traits, $i); ?>
 </tr>
 <tr class="hire_extra<?php if(!$hireable) echo ' hidden'; ?>"><td>Hire price in SD</td><td><input class="width100" type='text' name='hire_sd' value="<?php echo $hire_sd; ?>" /></td>
-<?php
-if($i < $num) {
-	echo '<td class="width125">' . $traits[$i]->getTitle() . '</td><td class="width50"> 
-	<div class="color-box" style="background-color: #' . $traits[$i]->getColor() . '"></div></td>
-	<td>' . $traits[$i]->getColor() .'</td>
-	<td>MOVE</td>';
-	$i++;
-} 
-?>
+<?php $i = showTrait($num, $traits, $i); ?>
 </tr>
 <tr class="hire_extra<?php if(!$hireable) echo ' hidden"'; ?>"><td>Hire price in items</td><td>
 <input text="text" class="width100" name="hire_amount" value="<?php echo $hire_amount; ?>" /> <select size="1" name="hire_item"><?php replace($hire_item, $item_options); ?></select>
 </td>
-<?php
-if($i < $num) {
-	echo '<td class="width125">' . $traits[$i]->getTitle() . '</td><td class="width50"> 
-	<div class="color-box" style="background-color: #' . $traits[$i]->getColor() . '"></div></td>
-	<td>' . $traits[$i]->getColor() .'</td>
-	<td>MOVE</td>';
-	$i++;
-} 
-?>
+<?php $i = showTrait($num, $traits, $i); ?>
 </tr>
 
-<tr><td>Available for breeding?</td><td><input type='radio' name="breedable" class='breedable' value='y'<?php checked($breedable); ?> /> Yes <input name="breedable" class='breedable' type='radio' value='n'<?php checked(!$breedable); ?> /> No</td></tr>
-<tr class="breed_extra<?php if(!$breedable) echo ' hidden"'; ?>"><td>Breeding price in SD</td><td><input class="width100" type='text' name='breed_sd' value="<?php echo $breed_sd; ?>" /></td></tr>
-<tr class="breed_extra<?php if(!$breedable) echo ' hidden"'; ?>"><td>Breeding price in items</td><td>
-<input text="text" class="width100" name="breed_amount" value="<?php echo $breed_amount; ?>" /> <select size="1" name="breed_item"><?php replace($breed_item, $item_options); ?></select>
-</td></tr>
+<tr>
+<td>Available for breeding?</td>
+<td><input type='radio' name="breedable" class='breedable' value='y'<?php checked($breedable); ?> /> Yes <input name="breedable" class='breedable' type='radio' value='n'<?php checked(!$breedable); ?> /> No</td>
+<?php $i = showTrait($num, $traits, $i); ?>
+</tr>
+<tr class="breed_extra<?php if(!$breedable) echo ' hidden"'; ?>">
+<td>Breeding price in SD</td>
+<td><input class="width100" type='text' name='breed_sd' value="<?php echo $breed_sd; ?>" /></td>
+<?php $i = showTrait($num, $traits, $i); ?>
+</tr>
+<tr class="breed_extra<?php if(!$breedable) echo ' hidden"'; ?>">
+<td>Breeding price in items</td>
+<td><input text="text" class="width100" name="breed_amount" value="<?php echo $breed_amount; ?>" /> 
+<select size="1" name="breed_item"><?php replace($breed_item, $item_options); ?></select>
+</td>
+<?php $i = showTrait($num, $traits, $i); ?>
+</tr>
 <tr><td class="text-center" colspan="2">
 <input class="submit-input" type='submit' name='update_squffy' value='Update <?php echo $name; ?>' />
-</td></tr>
+</td>
+<?php $i = showTrait($num, $traits, $i); ?>
+</tr>
 </table>
 </form>
 
@@ -134,5 +116,25 @@ if($i < $num) {
 function replace($val, $options) {
 	echo str_replace('value="' . $val . '"','value="' . $val . '" selected', $options);
 }
+
+function showTrait($num, &$traits, $i) {
+	if($i == $num) {
+		echo '<td colspan="4" class="text-center"><input type="submit" class="submit-input" value="Reorder traits" name="reorder" /></td>';
+	} elseif($i < $num) {
+		echo '<td class="width125">' . $traits[$i]->getTitle() . '</td><td class="width50"> 
+		<div class="color-box" style="background-color: #' . $traits[$i]->getColor() . '"></div></td>
+		<td class="width80">' . $traits[$i]->getColor() .'</td>
+		<td><img src="./images/icons/arrow_up.png" class="margin-right-small no-border float-left';
+		if($i == 0) { echo ' invisible'; }
+		echo '" />';
+		if($i < $num - 1) { echo '<img src="./images/icons/arrow_down.png" class="margin-right-small no-border float-left" />'; }
+		echo '</td>';
+	} else {
+		echo '<td></td><td></td><td></td><td></td>';
+	}
+	$i++;
+	return $i;
+}
+
 include('./includes/footer.php');
 ?>
